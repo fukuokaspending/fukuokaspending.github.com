c9308a7de77274929f40c39a585c2e5031df9ccc
Tajima Itsuro <tajima@georepublic.co.jp>
2013-03-05 11:39:18 +0900

    fixed bubbletree and use same icon on daily bread and bubbletree

--- js/cofog.js
+++ js/cofog.js
@@ -9,20 +9,20 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* fukuoka */
-  '1': { icon: 'icons/government.svg', color: '#C75746' },
-  '2': { icon: 'icons/admin.svg', color: '#C75746' },
-  '3': { icon: 'icons/community.svg', color: '#C75746' },
-  '4': { icon: 'icons/helping-others.svg', color: '#C75746' },
-  '5': { icon: 'icons/environment.svg', color: '#C75746' },
-  '6': { icon: 'icons/forest.svg', color: '#C75746' },
-  '7': { icon: 'icons/economy-tourism.svg', color: '#C75746' },
-  '8': { icon: 'icons/construction.svg', color: '#C75746' },
-  '9': { icon: 'icons/government.svg', color: '#C75746' },
-  '10': { icon: 'icons/harbor.svg', color: '#C75746' },
-  '11': { icon: 'icons/fire-brigade.svg', color: '#C75746' },
-  '12': { icon: 'icons/education.svg', color: '#C75746' },
-  '13': { icon: 'icons/housing.svg', color: '#C75746' },
-  '14': { icon: 'icons/public-debt.svg', color: '#C75746' },
-  '15': { icon: 'icons/water.svg', color: '#C75746' },
-  '16': { icon: 'icons/unknown.svg', color: '#C75746' }
+  '1': { icon: 'icons/government.svg', color: '#110076' },
+  '2': { icon: 'icons/admin.svg', color: '#110076' },
+  '3': { icon: 'icons/community.svg', color: '#110076' },
+  '4': { icon: 'icons/helping-others.svg', color: '#110076' },
+  '5': { icon: 'icons/environment.svg', color: '#110076' },
+  '6': { icon: 'icons/forest.svg', color: '#110076' },
+  '7': { icon: 'icons/economy-tourism.svg', color: '#110076' },
+  '8': { icon: 'icons/construction.svg', color: '#110076' },
+  '9': { icon: 'icons/government.svg', color: '#110076' },
+  '10': { icon: 'icons/harbor.svg', color: '#110076' },
+  '11': { icon: 'icons/fire-brigade.svg', color: '#110076' },
+  '12': { icon: 'icons/education.svg', color: '#110076' },
+  '13': { icon: 'icons/housing.svg', color: '#110076' },
+  '14': { icon: 'icons/public-debt.svg', color: '#110076' },
+  '15': { icon: 'icons/water.svg', color: '#110076' },
+  '16': { icon: 'icons/unknown.svg', color: '#110076' }
 };
