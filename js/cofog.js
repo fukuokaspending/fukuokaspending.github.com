39739e23c3d68a552864a873f29acb04e0f59f7f
Tajima Itsuro <tajima@georepublic.co.jp>
2013-03-05 11:02:07 +0900

    fixed icons and translated

--- js/cofog.js
+++ js/cofog.js
@@ -8,33 +8,21 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 
 OpenSpending.Styles.Cofog = {
 
-  /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746' },
-  '1.1': { icon: 'icons/helping-others.svg', color: '#C75746' },
-  '2': { icon: 'icons/schools.svg', color: '#C75746' },
-  '2.1': { icon: 'icons/pre-school.svg', color: '#C75746' },
-  '2.2': { icon: 'icons/education.svg', color: '#C75746' },
-  '3': { icon: 'icons/civilian-action.svg', color: '#C75746' },
-  '3.1': { icon: 'icons/civilian-action.svg', color: '#C75746' },
-  '4': { icon: 'icons/order-safety.svg', color: '#C75746' },
-  '4.1': { icon: 'icons/order-safety.svg', color: '#C75746' },
+  /* fukuoka */
+  '1': { icon: 'icons/government.svg', color: '#C75746' },
+  '2': { icon: 'icons/admin.svg', color: '#C75746' },
+  '3': { icon: 'icons/community.svg', color: '#C75746' },
+  '4': { icon: 'icons/helping-others.svg', color: '#C75746' },
   '5': { icon: 'icons/environment.svg', color: '#C75746' },
-  '5.1': { icon: 'icons/wind.svg', color: '#C75746' },
-  '5.2': { icon: 'icons/tree.svg', color: '#C75746' },
-  '6': { icon: 'icons/garbage.svg', color: '#C75746' },
-  '6.1': { icon: 'icons/garbage.svg', color: '#C75746' },
-  '7': { icon: 'icons/our-streets.svg', color: '#C75746' },
-  '7.1': { icon: 'icons/housing.svg', color: '#C75746' },
-  '7.2': { icon: 'icons/street-lights.svg', color: '#C75746' },
-  '7.3': { icon: 'icons/farms.svg', color: '#C75746' },
-  '8': { icon: 'icons/economy-tourism.svg', color: '#C75746' },
-  '8.1': { icon: 'icons/culture.svg', color: '#C75746' },
-  '8.2': { icon: 'icons/dollar.svg', color: '#C75746' },
-  '8.3': { icon: 'icons/harbor.svg', color: '#C75746' },
+  '6': { icon: 'icons/forest.svg', color: '#C75746' },
+  '7': { icon: 'icons/economy-tourism.svg', color: '#C75746' },
+  '8': { icon: 'icons/construction.svg', color: '#C75746' },
   '9': { icon: 'icons/government.svg', color: '#C75746' },
-  '9.1': { icon: 'icons/publicaffairs.svg', color: '#C75746' },
-  '9.2': { icon: 'icons/government.svg', color: '#C75746' },
-  '10': { icon: 'icons/traffic-watersup.svg', color: '#C75746' },
-  '10-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746' },
-  '13': { icon: 'icons/money.svg', color: '#C75746' }
+  '10': { icon: 'icons/harbor.svg', color: '#C75746' },
+  '11': { icon: 'icons/fire-brigade.svg', color: '#C75746' },
+  '12': { icon: 'icons/education.svg', color: '#C75746' },
+  '13': { icon: 'icons/housing.svg', color: '#C75746' },
+  '14': { icon: 'icons/public-debt.svg', color: '#C75746' },
+  '15': { icon: 'icons/water.svg', color: '#C75746' },
+  '16': { icon: 'icons/unknown.svg', color: '#C75746' }
 };
